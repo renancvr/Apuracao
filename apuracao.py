a57e784d9cb76f2337c00aaecf5e7374f4ab53db
Renan Costa <renancvr20@gmail.com>
2022-10-02 17:43:41 -0300

    Update apuracao.py

--- apuracao.py
+++ apuracao.py
@@ -7,6 +7,9 @@ print("Executado em =", current_time)
 
 df = pd.read_json('https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json')
 
+print("Urnas apuradas: {urnas}%".format(urnas = df['pesi'][0]))
+
+
 for i in range(11):
 	string = ("Nome: {name} ".format(name = df['cand'][i]['nm']))
 	string += ("Votos: {votes} ".format(votes = df['cand'][i]['vap']))
