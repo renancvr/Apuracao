a3a61976e71fd157b42f10c8f33b4fcf0ca3290d
Renan Costa <renancvr20@gmail.com>
2022-10-02 17:19:18 -0300

    Add files via upload

---
+++ apuracao.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from datetime import datetime
+
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+print("Executado em =", current_time)
+
+df = pd.read_json('https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json')
+
+for i in range(11):
+	string = ("Nome: {name} ".format(name = df['cand'][i]['nm']))
+	string += ("Votos: {votes} ".format(votes = df['cand'][i]['vap']))
+	string += ("Percentual: {percent}\n".format(percent = df['cand'][i]['pvap']))
+	print(string)
+	string = ''
